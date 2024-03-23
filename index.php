058a001eaaa3d030dac8012f72e9b4acfb8d3eb6
vestrylyestin <vestrylyestin@gmail.com>
2024-03-23 14:12:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -589,10 +589,9 @@
         <!--Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> -->
       <!--</div>
     </div>-->
+    
   </footer><!-- End Footer -->
-  
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
+  <a href="https://rumahpintarinovasi.com" class="align-items-center justify-content-center">Developed By Rumah Pintar Teknologi & Inovaitif</a>
   <!-- Vendor JS Files -->
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
